8b6eec2a2678ca5f415eaf175641aa11b32ebd28
S. Sanneman <seandafydd@gmail.com>
2020-08-16 14:13:56 -0700

    finished category API route

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -8,26 +8,25 @@ const ProductTag = require("./ProductTag");
 Product.belongsTo(Category, {
   foreignKey: "category_id",
 }),
-// Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: "category_id",
-}),
-// Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  through: ProductTag,
-  as: "tags",
-  foreignKey: "product_id",
-}),
-// Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
-  through: ProductTag,
-  as: "products",
-  foreignKey: "tag_id",
-}),
-
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+  // Categories have many Products
+  Category.hasMany(Product, {
+    foreignKey: "category_id",
+  }),
+  // Products belongToMany Tags (through ProductTag)
+  Product.belongsToMany(Tag, {
+    through: ProductTag,
+    as: "tags",
+    foreignKey: "product_id",
+  }),
+  // Tags belongToMany Products (through ProductTag)
+  Tag.belongsToMany(Product, {
+    through: ProductTag,
+    as: "products",
+    foreignKey: "tag_id",
+  }),
+  (module.exports = {
+    Product,
+    Category,
+    Tag,
+    ProductTag,
+  });
