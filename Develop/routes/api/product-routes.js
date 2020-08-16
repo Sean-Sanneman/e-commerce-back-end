bcecfc2f12ebcd42e449bb610b64baad00da541f
S. Sanneman <seandafydd@gmail.com>
2020-08-16 16:05:56 -0700

    started product api route, saving progress

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -8,7 +8,17 @@ router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   Product.findAll({
-    
+    include: [
+      {
+        model: Category,
+        as: "categories",
+        attributes: category_name,
+      },
+      {
+        model: Tag,
+        
+      }
+    ]
   })
 });
 
