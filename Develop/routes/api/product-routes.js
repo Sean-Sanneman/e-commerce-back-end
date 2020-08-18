80bd384664bc1d601280a1dc4aa2684a7ad6423d
S. Sanneman <seandafydd@gmail.com>
2020-08-18 03:01:20 -0700

    updated product routes. Post works, get does not

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -8,11 +8,17 @@ router.get("/", (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   Product.findAll({
+    attributes: ["id", "product_name", "price", "stock"],
     include: [
-      Category,
+      {
+        model: Category,
+        attributes: ["id", "category_name"],
+      },
       {
         model: Tag,
+        attributes: ["tag_name"],
         through: ProductTag,
+        as: "tags",
       },
     ],
   })
@@ -60,8 +66,7 @@ router.post("/", (req, res) => {
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
-    category_id: req.body.category_id,
-    tagIds: [req.body.tag_Ids],
+    tagIds: req.body.tag_Ids,
   })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -145,11 +150,9 @@ router.delete("/:id", (req, res) => {
   })
     .then((dbProData) => {
       if (!dbProData) {
-        res
-          .status(404)
-          .json({
-            message: "There appears to be no products associated with this id",
-          });
+        res.status(404).json({
+          message: "There appears to be no products associated with this id",
+        });
         return;
       }
       res.json(dbProData);
